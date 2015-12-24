6679ede1e1938c1b0c1408c9c467ef1b462bc36d
wacton <waacton@gmail.com>
2015-12-24 10:16:23 +0000

    Update to Tovarisch 1.0.0

--- Japangolin.Domain/Properties/AssemblyInfo.cs
+++ Japangolin.Domain/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Japangolin.Domain")]
+[assembly: AssemblyTitle("Wacton.Japangolin.Domain")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Japangolin.Domain")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCompany("Wacton")]
+[assembly: AssemblyProduct("Wacton.Japangolin.Domain")]
+[assembly: AssemblyCopyright("Copyright © Wacton 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
