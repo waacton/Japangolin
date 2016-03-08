b81e2b87fdcd039c6d53bcc9fbab5fbfb7c8188c
wacton <waacton@gmail.com>
2016-03-08 12:24:59 +0000

    Update phrase function moved to external JS file

---
+++ Japangolin.Web/wwwroot/js/functions.js
@@ -0,0 +1,19 @@
+﻿function updatePhrase(window, document) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            var japanesePhrase = JSON.parse(xhttp.responseText);
+            document.title = "Japangolin | " + japanesePhrase.Kana;
+            document.getElementById("kana").innerHTML = japanesePhrase.Kana;
+
+            var meaningsHtml = "";
+            japanesePhrase.Meaning.forEach(function(item) {
+                meaningsHtml += "<p>" + item + "</p>";
+            });
+            document.getElementById("meanings").innerHTML = meaningsHtml;
+        }
+    };
+
+    xhttp.open("GET", "api/random?t=" + Math.random(), false); // unique ID to avoid caching (IE...)
+    xhttp.send();
+}
