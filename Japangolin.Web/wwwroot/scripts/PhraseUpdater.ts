15203a816bd5c17fa7aeaea841f014bc6f72a8ff
wacton <waacton@gmail.com>
2016-03-09 13:31:37 +0000

    TypeScript PhraseUpdater class

---
+++ Japangolin.Web/wwwroot/scripts/PhraseUpdater.ts
@@ -0,0 +1,21 @@
+﻿class PhraseUpdater {
+    update(window: Window, document: Document){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = () => {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                var japanesePhrase = JSON.parse(xhttp.responseText);
+                document.title = `Japangolin | ${japanesePhrase.Kana}`;
+                document.getElementById("kana").innerHTML = japanesePhrase.Kana;
+
+                var meaningsHtml = "";
+                japanesePhrase.Meaning.forEach(item => {
+                    meaningsHtml += `<p>${item}</p>`;
+                });
+                document.getElementById("meanings").innerHTML = meaningsHtml;
+            }
+        };
+
+        xhttp.open("GET", `api/random?t=${Math.random()}`, false); // unique ID to avoid caching (IE...)
+        xhttp.send();
+    }
+}
