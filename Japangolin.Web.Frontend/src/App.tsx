ba08e44526d8b179471125c6016066988e4f396a
Wacton <waacton@gmail.com>
2021-12-26 21:43:46 +0000

    Update icon

--- Japangolin.Web.Frontend/src/App.tsx
+++ Japangolin.Web.Frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css';
-import logo from './Japangolin-round.png';
+import logo from './Japangolin-swirl.png';
 
 import JapangolinTest from "./components/JapangolinTest";
 
 function App() {
   return (
       <div className="App" style={{background: "#FAFAFA", height: "100vh", display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center"}}>
-          <div style={{background: "#404040", height: "25vmin", width: "25vmin", display: "flex", justifyContent: "center", alignItems: "center"}}>
+          <div style={{background: "#404046", height: "25vmin", width: "25vmin", display: "flex", justifyContent: "center", alignItems: "center"}}>
               <img src={logo} className="App-logo" alt="logo" style={{height: "20vmin"}} />
           </div>
 
-          <JapangolinTest textColor={"#404040"} />
+          <JapangolinTest textColor={"#404046"} />
       </div>
   );
 }
