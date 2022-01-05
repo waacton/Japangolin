71026b837c663767d981cc75f1e3a8fee56096a7
Wacton <waacton@gmail.com>
2022-01-05 14:33:30 +0000

    Remove unused logo

--- Japangolin.Web.Frontend/src/components/logo.tsx
+++ Japangolin.Web.Frontend/src/components/logo.tsx
@@ -1,10 +1,15 @@
-import logo from "../Japangolin-swirl.png";
+import logo from "../Japangolin-swirl-logo.png";
 
 import { Stack } from "@mui/material";
 
 function Logo() {
   return (
-    <Stack direction={"row"} justifyContent={"center"} alignItems={"center"} sx={{ bgcolor: "#404046", height: 40, width: 40 }}>
+    <Stack
+      direction={"row"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      sx={{ bgcolor: "#404046", height: 40, width: 40 }}
+    >
       <img src={logo} className="App-logo" alt="logo" height={24} />
     </Stack>
   );
