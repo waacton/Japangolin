fa676cbb49d260d00a8cd4dd9d0c14ecb276d157
Wacton <waacton@gmail.com>
2023-02-05 21:28:56 +0000

    Update frontend proxy

--- Japangolin.Web/Frontend/src/setupProxy.js
+++ Japangolin.Web/Frontend/src/setupProxy.js
@@ -6,7 +6,7 @@ const context = [
 
 module.exports = function (app) {
     const appProxy = createProxyMiddleware(context, {
-        target: 'https://localhost:7890',
+        target: 'https://localhost:7443',
         secure: false
     });
 
